fb37227f5f22ffd32a94aeaff0a160588c828abd
jacalvar <jacalvar@microsoft.com>
2024-01-22 17:29:13 +0100

    Unconditionally enable the regex during development (linker will still trim it unless enabled on the project)

--- src/Components/Components/src/Routing/RegexConstraintSupport.cs
+++ src/Components/Components/src/Routing/RegexConstraintSupport.cs
@@ -5,6 +5,11 @@ namespace Microsoft.AspNetCore.Components.Routing;
 
 internal static class RegexConstraintSupport
 {
-    public static bool IsEnabled =>
-        AppContext.TryGetSwitch("Microsoft.AspNetCore.Components.Routing.RegexConstraintSupport", out var enabled) && enabled;
+    // We should check the AppContext switch in the implementation, but it doesn't flow to the wasm runtime
+    // during development, so we can't offer a better experience (build time message to enable the switch)
+    // until the context switch flows to the runtime.
+    // This value gets updated by the linker when the app is trimmed, so the code will always be removed from
+    // webassembly unless the switch is enabled.
+    public static bool IsEnabled => true;
+        // AppContext.TryGetSwitch("Microsoft.AspNetCore.Components.Routing.RegexConstraintSupport", out var enabled) && enabled;
 }
